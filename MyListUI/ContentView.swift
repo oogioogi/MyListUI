798ae55034878b0d4bdd6cb7ef05593797a3f3c8
oogioogi <detacl73@gmail.com>
2021-06-22 12:44:47 +0900

    Initial Commit

---
+++ MyListUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyListUI
+//
+//  Created by 이용석 on 2021/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
